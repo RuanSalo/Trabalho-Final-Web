1554d339a611678e4978050113b0837da31aa40e
RuanSalo <ruansalo03@gmail.com>
2021-06-18 01:09:24 -0300

    Modificações no layout implementadas

--- trabalho-final/start/routes.ts
+++ trabalho-final/start/routes.ts
@@ -3,6 +3,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', 'HomeController.index')
 Route.get('about', 'HomeController.about')
 
+
 Route.group(() => {
     Route.resource('rifas', 'RifasController')
 }).middleware('auth')
@@ -12,5 +13,6 @@ Route.post('/register', 'AuthController.store').as('auth.store')
 Route.get('/login', 'AuthController.login').as('auth.login')
 Route.post('/login', 'AuthController.verify').as('auth.verify')
 Route.get('/logout', 'AuthController.logout').as('auth.logout')
+
 
 
